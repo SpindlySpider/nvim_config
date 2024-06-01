25a4dbc7d4c413eac01f304bdaf8125a6cfc134a
SpindlySpider <kaihrmt@gmail.com>
2024-06-02 00:20:38 +0100

    added lazy git and floating menu

---
+++ lua/custom/plugins/vim-float.lua
@@ -0,0 +1,17 @@
+local float = {
+  "voldikss/vim-floaterm",
+  name = "vim float",
+  config = function()
+    vim.keymap.set('n', '<leader>go', function()
+        vim.cmd(":FloatermNew --height=0.9 --width=0.9 --floaterm_title=git lazygit")
+        print("git open")
+      end,
+      { desc = '[g]it [o]ptions -  open lazy git in a floating window' })
+  end,
+  opts = {
+    floaterm_title = "lazy git",
+    floaterm_width = 0.9,
+    floaterm_height = 0.9,
+  }
+}
+return float
