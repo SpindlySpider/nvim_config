27f3c68fac357dd02dda8bbe309a3c8539cdb702
spindly <kaihrmt@gmail.com>
2025-01-27 14:46:29 +0000

    Update remote

--- lua/custom/plugins/remote-nvim.lua
+++ lua/custom/plugins/remote-nvim.lua
@@ -1,6 +1,6 @@
 return {
    "amitds1997/remote-nvim.nvim",
-   version = "*", -- Pin to GitHub releases
+   version = "v0.3.9", -- Pin to 0.3.9 to allow transfer
    dependencies = {
        "nvim-lua/plenary.nvim", -- For standard functions
        "MunifTanjim/nui.nvim", -- To build the plugin UI
