3603307e815fbcd86fd3c762b5e6b8d744e50f30
spindly <kaihrmt@gmail.com>
2025-02-04 11:01:00 +0000

    Added folds

--- lua/custom/plugins/nvim_ufo.lua
+++ lua/custom/plugins/nvim_ufo.lua
@@ -1,6 +1,18 @@
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 
 return {
-  "kevinhwang91/nvim-ufo",
-  event = "VeryLazy",
-  requires ='kevinhwang91/promise-async'
+    "kevinhwang91/nvim-ufo",
+    keys={
+        {"<leader>j",function() require("ufo").openAllFolds() end, desc="Open fold"},
+        {"<leader>k",function() require("ufo").closeAllFolds() end, desc="Close fold"}
+    },
+    opts = {
+        provider_selector = function(bufnr, filetype, buftype)
+            return { 'treesitter', 'indent' }
+        end
+    },
+    dependencies = 'kevinhwang91/promise-async',
 }
